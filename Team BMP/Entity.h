9310edc6c6fccd0ce61e6946afc12caeabf2eb81
beermanforlife <beermanforlife@gmail.com>
2011-03-09 01:06:01 +0000

    #Added a basic Entity and tried to get a sprite to draw and update but for some reason even while looking at an older project as reference i cannot get anything other then a black screen. If someone else wants to check it out please do.

---
+++ Team BMP/Entity.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "Sprite.h"
+
+#define NUM_STATS		9
+#define NUM_LOCATIONS	2
+#define CURRENT2MAX		2
+#define NUM_RESISTANCES	3
+
+#define SCREENLOC		0
+#define WORLDLOC		1
+
+#define FIRE			0
+#define	ICE				1
+#define	LIGHTNING		2
+
+#define CENTERSCREENX	400
+#define CENTERSCREENY	300
+//each array in this next area the first part is current and second is max
+struct Stats
+{
+	int health[CURRENT2MAX];
+	int energy[CURRENT2MAX];
+	int strength;
+	int intellect;
+	int defence;
+	int resistances[NUM_RESISTANCES];//fire/ice/lightning
+};
+struct Location
+{
+	int x;
+	int y;
+};
+enum e_eType{player, minion, boss};
+
+class Entity
+{
+private:
+	Stats m_stats;
+	Location m_locations[NUM_LOCATIONS];//0 is screen, 1 is world
+	e_eType m_myType;
+	Sprite * m_mySprite;
+public:
+	Entity(){
+		m_stats.defence = m_stats.intellect = m_stats.strength = 0;
+		for(int i = 0; i < CURRENT2MAX; i++)
+			m_stats.energy[i] = m_stats.health[i] = 1;
+		for(int i = 0; i < NUM_RESISTANCES; i ++)
+			m_stats.resistances[i] = 0;
+	}
+	Entity(int a_def, int a_int, int a_str, int a_health, int a_energy, int a_fRes, int a_iRes, int a_lRes, Sprite a_sprite)
+	{
+		m_stats.defence = a_def;
+		m_stats.intellect = a_int;
+		m_stats.strength = a_str;
+		for(int i = 0; i < CURRENT2MAX; i++)
+		{
+			m_stats.energy[i] = a_energy;
+			m_stats.health[i] = a_health;
+		}
+		m_stats.resistances[FIRE] = a_fRes;
+		m_stats.resistances[ICE] = a_iRes;
+		m_stats.resistances[LIGHTNING] = a_lRes;
+		m_mySprite = &a_sprite;
+		m_locations[SCREENLOC].x = CENTERSCREENX;
+		m_locations[SCREENLOC].y = CENTERSCREENY;
+	}
+	Stats getStats(){return m_stats;}
+	Location getLocationScreen(){return m_locations[SCREENLOC];}
+	Location getLocationWorld(){return m_locations[WORLDLOC];}
+	void update(int a_time)
+	{
+		m_mySprite->update(a_time);
+	}
+	void draw(SDL_Surface * a_screen)
+	{
+		m_mySprite->draw(a_screen, m_locations[SCREENLOC].x,m_locations[SCREENLOC].y); 
+	}
+};
--- Team BMP/Main.cpp
+++ Team BMP/Main.cpp
@@ -3,6 +3,7 @@
 #include "spoint.h"
 #include "UserInput.h"
 #include "Chip.h"
+#include "Entity.h"
 
 //Some debugging includes
 #include <stdio.h>
@@ -10,6 +11,7 @@
 #define WIDTH		800
 #define HEIGHT		600
 #define BPP			32
+#define FRAMEDELAY	20;
 
 int main(int argc, char ** argv)//must be the header for sdl application and yes the arg stuff
 {
@@ -17,18 +19,41 @@ int main(int argc, char ** argv)//must be the header for sdl application and yes
 
 
 	SDL_Init(SDL_INIT_EVERYTHING);
-//	SDL_Surface * screen =
-	SDL_SetVideoMode(WIDTH, HEIGHT, BPP, SDL_SWSURFACE);
+	SDL_Surface * screen = 
+		SDL_SetVideoMode(WIDTH, HEIGHT, BPP, SDL_SWSURFACE);
 	SDL_Event e;
 	UserInput ui; //This is the UserInput class.
+
+	//Timer stuff, for updateing sprites and anything else
+	Uint32 then = SDL_GetTicks(), now, soon, passed;
+
 	//the user interface variable
 	bool running = true;
 
 	//Some debugging for the UI.
 	bool change = false;
 
+	//loading asprite to see if it works with the entity
+	Sprite test("spriteTest.bmp", 3, 32);
+	test.setTransparency(255,0,255);
+	test.setHIndex(4,3);
+	Entity eTest(0,0,0,0,0,0,0,0,test);
+
 	while(running) //While loop, can be easily used for testing.
 	{
+		//update the time
+		now = SDL_GetTicks();
+		passed = now - then;
+		then = now;
+		soon = now + FRAMEDELAY;
+
+		//update
+		test.update(passed);
+
+
+
+
+		//input
 		while(SDL_PollEvent(&e)) //Polls the events of SDL
 		{
 			switch(e.type) //If there's an event, then what type?
@@ -51,10 +76,16 @@ int main(int argc, char ** argv)//must be the header for sdl application and yes
 		}
 		if(change)
 		{
-			printf("%d, %d, Button is: %d, Key is: %c \n", ui.getMouseX(), ui.getMouseY(), ui.getClick(), ui.getKey());
+			//printf("%d, %d, Button is: %d, Key is: %c \n", ui.getMouseX(), ui.getMouseY(), ui.getClick(), ui.getKey());
 			change = false;
 		}
+				//draw
+		test.draw(screen,100,100);
+		SDL_Flip(screen);
+
+		SDL_Delay(100);
 	}
+	SDL_FreeSurface(screen);
 	SDL_Quit();//must do to free the memory that was used for init
 	
 	
