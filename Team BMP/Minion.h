a5747d587c0302c5b8fe0af27dd87826a2f50948
jparks.gsp <jparks.gsp@gmail.com>
2011-04-02 00:57:45 +0000

    * made Minion more lively - constantly wanders - sprite changes row depending on direction * added basic blunt sprite

--- Team BMP/Minion.h
+++ Team BMP/Minion.h
@@ -1,22 +1,22 @@
 #pragma once
 
 #include "Entity.h"
-enum e_wander {WANDER_MAX = 100, WANDER_DIST = 50};
+#define	WANDER_DIST	50
 enum e_states	{WANDER};
 
 class Minion : public Entity
 {
 private:
-	int lastDirectionChange, m_state;
+	int m_lastDirectionChange, m_state;
 public:
 	Minion(){}
 	Minion(int a_def, int a_int, int a_str, int a_health, int a_energy, int a_fRes, int a_iRes, int a_lRes, SDL_Sprite* a_sprite)
 		:Entity(a_def, a_int, a_str, a_health, a_energy, a_fRes, a_iRes, a_lRes, a_sprite)
 	{
 		m_eType = MINION;
-		lastDirectionChange = 0;
 		m_state = WANDER;
 		m_target.set(m_location);
+		m_lastDirectionChange = TIME_WANDER;
 	}
 	void respawn()
 	{
@@ -31,14 +31,32 @@ public:
 	}
 	void wander(int a_timePassed)
 	{
-		lastDirectionChange += a_timePassed;
-		if(lastDirectionChange > TIME_WANDER)
+		m_lastDirectionChange += a_timePassed;
+		if(m_lastDirectionChange >= TIME_WANDER || m_location.equals(m_target))
 		{
-			int tX, tY;
-			tX = m_location.getX()+((rand()%WANDER_MAX)-WANDER_DIST);//gives a + or - 50 change in wander
-			tY = m_location.getY()+((rand()%WANDER_MAX)-WANDER_DIST);
-			m_target.set(tX, tY);
-			lastDirectionChange = 0;
+			m_lastDirectionChange = 0;
+			//gives a + or - 50 change in wander
+			m_target.setX(m_location.getX()+((rand()%(WANDER_DIST*2+1))-WANDER_DIST));
+			m_target.setY(m_location.getY()+((rand()%(WANDER_DIST*2+1))-WANDER_DIST));
+			if(m_target.x < m_location.x)
+				m_sprite->setRIndex(3);
+			else
+			{
+				if(m_target.x > m_location.x)
+				{
+					if(m_target.y < m_location.y)
+						m_sprite->setRIndex(1);
+					else
+						m_sprite->setRIndex(2);
+				}
+				else
+				{
+					if(m_target.y < m_location.y)
+						m_sprite->setRIndex(0);
+					else
+						m_sprite->setRIndex(2);
+				}
+			}
 		}
 		else
 			moveToTarget(SPEED_MINION);
