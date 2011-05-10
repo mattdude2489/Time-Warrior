9b98a434a42fde94a71192d98b16e88c753d2afe
ebcub79 <ebcub79@gmail.com>
2011-05-10 22:21:49 +0000

    Completed Base_Leveler class except for checking if upgradeable | not integrated

--- Team BMP/BaseLeveler.cpp
+++ Team BMP/BaseLeveler.cpp
@@ -2,8 +2,128 @@
 
 BaseLeveler::BaseLeveler(void)
 {
+	LS_Init();
+	//intitalize the XP_STATS variable Divine
+	divineBlock.attack_level = STARTING_LV;
+	divineBlock.attack_type = DIVINE;
+	divineBlock.total_XP = STARTING_XP;
+	divineBlock.upgradeable = false;
+
+	//Storm
+	lightningBlock.attack_level = STARTING_LV;
+	lightningBlock.attack_type = STORM;
+	lightningBlock.total_XP = STARTING_XP;
+	lightningBlock.upgradeable = false;
+	//fire
+	fireBlock.attack_level = STARTING_LV;
+	fireBlock.attack_type = FIRE;
+	fireBlock.total_XP = STARTING_XP;
+	fireBlock.upgradeable = false;
+	//ice
+	iceBlock.attack_level = STARTING_LV;
+	iceBlock.attack_type = ICE;
+	iceBlock.total_XP = STARTING_XP;
+	iceBlock.upgradeable = false;
+	//melee
+	meleeBlock.attack_level = STARTING_LV;
+	meleeBlock.attack_type = MELEE;
+	meleeBlock.total_XP = STARTING_XP;
+	meleeBlock.upgradeable = false;
 }
 
 BaseLeveler::~BaseLeveler(void)
 {
+	delete[] XP_KEY;
+}
+/*--- Base Functions ---*/
+/*--- XP Gain Functions ---*/
+void BaseLeveler::GainMagicXP(int chipBaseType)
+{
+	switch(chipBaseType)
+	{
+	case DIVINE:
+		divineBlock.total_XP++;
+		if(isLevelUp((int)DIVINE))
+			divineBlock.attack_level++;
+		break;
+	case STORM:
+		lightningBlock.total_XP++;
+		if(isLevelUp((int)STORM))
+			lightningBlock.attack_level++;
+		break;
+	case FIRE:
+		fireBlock.total_XP++;
+		if(isLevelUp((int)FIRE))
+			fireBlock.attack_level++;
+		break;
+	case ICE:
+		iceBlock.total_XP++;
+		if(isLevelUp((int)ICE))
+			iceBlock.attack_level++;
+		break;
+	}//end switch
+}
+
+void BaseLeveler::GainMeleeXP()
+{
+	meleeBlock.total_XP++;
+	if(isLevelUp((int)MELEE))
+		meleeBlock.attack_level++;
+}
+/*--- Level checking fuctions ---*/
+/*--- Accessor/Mutators ---*/
+/*--- Main Functions ---*/
+void BaseLeveler::LS_Init()
+{
+	XP_KEY = new LEVEL_SYSTEM[40];
+	for(int i = 0; i < MAX_ATK_LVL; i++)
+	{
+		if(i == 0)
+		{
+			XP_KEY[0].level = STARTING_LV;
+			XP_KEY[0].xpRequired = STARTING_XP;
+		}
+		else
+		{
+			XP_KEY[i].level = (i + 1);
+			XP_KEY[i].xpRequired += XP_KEY[i].level;
+		}//end else
+	}// end for
+}
+
+bool BaseLeveler::isLevelUp(int type)
+{
+	switch(type)
+	{
+	case DIVINE:
+		if(divineBlock.total_XP >= XP_KEY[divineBlock.attack_level + 1].xpRequired)
+			return true;
+		else
+			return false;
+		break;
+	case STORM:
+		if(lightningBlock.total_XP >= XP_KEY[lightningBlock.attack_level + 1].xpRequired)
+			return true;
+		else
+			return false;
+		break;
+	case FIRE:
+		if(fireBlock.total_XP >= XP_KEY[fireBlock.attack_level + 1].xpRequired)
+			return true;
+		else
+			return false;
+		break;
+	case ICE:
+		if(iceBlock.total_XP >= XP_KEY[iceBlock.attack_level + 1].xpRequired)
+			return true;
+		else
+			return false;
+		break;
+	case MELEE:
+		if(meleeBlock.total_XP >= XP_KEY[meleeBlock.attack_level + 1].xpRequired)
+			return true;
+		else
+			return false;
+		break;
+	}//end switch
 }
--- Team BMP/BaseLeveler.h
+++ Team BMP/BaseLeveler.h
@@ -3,6 +3,10 @@
 This class handles XP Gain and leveling for
 Characters (15), Melee Attacks (40), Magic Attacks (40)
 */
+const int STARTING_XP = 0;
+const int STARTING_LV = 1;
+const int MAX_ATK_LVL = 40;
+const int MAX_CHAR_LV = 15;
 
 /*XP Gain Formula | f(n){n > 0} = f(n-1) + n
 f(1) = 0 + 1 = 1
@@ -17,6 +21,40 @@ struct LEVEL_SYSTEM
 	int xpRequired;
 };
 
+// hold the type, total XP, and current level of attack
+struct XP_STATS
+{
+	bool upgradeable;
+	int attack_type;
+	int attack_level;
+	int total_XP;
+};
+
+// the base attack types
+enum BASE_TYPE
+{
+	DIVINE = 0,
+	STORM,
+	FIRE,
+	ICE,
+	MELEE,
+	DIVINE_1,
+	STORM_1,
+	FIRE_1,
+	ICE_1,
+	MELEE_1,
+	DIVINE_2,
+	STORM_2,
+	FIRE_2,
+	ICE_2,
+	MELEE_2,
+	DIVINE_3,
+	STORM_3,
+	FIRE_3,
+	ICE_3,
+	MELEE_3
+};
+
 class BaseLeveler
 {
 public:
@@ -25,13 +63,26 @@ public:
 	~BaseLeveler(void);
 	/*--- Base Functions ---*/
 	/*--- XP Gain Functions ---*/
+	void GainMagicXP(int chipBaseType);
+	void GainMeleeXP();
 	/*--- Level checking fuctions ---*/
-	//check the current level and XP if, XP
+	// check if the level allows for an upgrade
+	void upgradeAttack(XP_STATS statBlock){}
 	/*--- Accessor/Mutators ---*/
+	//check if the the attack can be upgraded (every 10 levels)
+	void isUpgradeable(XP_STATS statBlock){}
 private:
 	/*--- Main Functions ---*/
+	void LS_Init();
+	//check the current level and XP if, XP
+	bool isLevelUp(int type);
 	/*--- Outside Variables ---*/
 	/*--- Class Variables ---*/
 	// This goes up to L40
 	LEVEL_SYSTEM *XP_KEY;
+	XP_STATS divineBlock;
+	XP_STATS lightningBlock;
+	XP_STATS fireBlock;
+	XP_STATS iceBlock;
+	XP_STATS meleeBlock;
 };
